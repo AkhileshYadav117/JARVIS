8dc877783d18f5efa6d148d7d99e870502a22a2f
AkhileshYadav117 <yadavakhilesh.org@gmail.com>
2026-08-07 08:08:05 +0530

    Add JARVIS source code

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "song" : "https://youtu.be/Zg54dEYNegA?si=Plq0yOT3X_w4mb7J",
+    "hello" : "https://youtu.be/igQCv_Y33NI?si=QmUxkiqQoXbvBtu3",
+    "star":"https://youtu.be/dl-HCf4TBqk?si=VFhFXI1IGlxmTtpY",
+    "gehra":"https://youtu.be/GX9x62kFsVU?si=DyM6pPfDDrIL4X0h",
+    "dhurandhar":"https://youtu.be/OVHkruv-dg8?si=Ie4SsWyu8m68otDK"   
+}
